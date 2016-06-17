18130f72b6afd94bdf13877d68e47ea29d1443bd
Vladimir Chemeris <chamav@gmail.com>
2016-06-17 22:10:35 +0500

    first test

--- tests/MainTest.php
+++ tests/MainTest.php
@@ -4,16 +4,17 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class MainTest extends TestCase
 {
     /**
      * A basic functional test example.
      *
      * @return void
      */
-    public function testBasicExample()
+    public function testBasic()
     {
         $this->visit('/')
-             ->see('Laravel 5');
+            ->see('Welcome')
+            ->dontSee('chama');
     }
 }
