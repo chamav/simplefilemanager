326bcfc62a313ba8ea9202888c6bd796b2f470ed
Vladimir Chemeris <chamav@gmail.com>
2016-06-17 14:44:56 +0500

    added userfiles controller and routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
+
+Route::get('/files', 'UserFilesController@index');
+Route::post('/files', 'UserFilesController@store');
+Route::delete('/files/{task}', 'UserFilesController@destroy');
