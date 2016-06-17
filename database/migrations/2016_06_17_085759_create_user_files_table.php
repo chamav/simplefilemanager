8dafd346425793a6d45be1c9574b3cb11a5f82f5
Vladimir Chemeris <chamav@gmail.com>
2016-06-17 14:09:45 +0500

    added user_files table

---
+++ database/migrations/2016_06_17_085759_create_user_files_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_files', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->string('hash', 200);
+            $table->bigInteger('size',false, true);
+            $table->integer('user_id')->index();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('user_files');
+    }
+}
